33eccf78891aea44291e195da37d2ba5109b833b
Lakshya Dhingra <dlakshya223@gmail.com>
2024-12-31 21:01:29 +0530

    Basic logic building and importing hotels data

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
+import pandas as pd
+
+df = pd.read_csv("hotels.csv")
+
+
 class Hotel:
+    def __init__(self, id):
+        self.id = id
+
     def book(self):
+        pass
+
+    def available(self):
         pass
 
 
 class ReservationTicket:
+    def __init__(self, username, hotel):
+        self.username = username
+        self.hotel = hotel
+
     def generate_ticket(self):
-        pass
+        content = f"{username}, your ticket for {hotel} has been generated successfully"
+        return content
+
+
+print(df)
+username = input("Enter your name: ")
+hotel_id_input = input("Enter hotel ID:")
+hotel = Hotel(id)
+reservation_ticket = ReservationTicket(username, hotel)
+if hotel.available():
+    hotel.book()
+    print(reservation_ticket.generate_ticket())
+else:
+    print('Hotel is currently unavailable')
